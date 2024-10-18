0c12d242f6251a82fb153188ab6c1e3ce31f161d
SRAAWANSAI\sraawan <sraawan.sai@gmail.com>
2024-10-18 08:12:06 +0530

    hearders change in user,js for ngrox

--- routers/user.js
+++ routers/user.js
@@ -6,9 +6,9 @@ const jwt = require('jsonwebtoken')
 //const verifyToken = require("../middleware/authverfiy")
 
 
-router.get("/",headers: {
+router.get("/",{headers: {
   'ngrok-skip-browser-warning': 'true'
-},async (req, res) => {
+}},async (req, res) => {
   const user = await User.find();
 
   if (!user) return res.status(404).json("user data not fond");
